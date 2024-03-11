e59cfcbbffa4661a5d55a3d2536a404463339891
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-11 13:45:41 +0200

    [desktop] Chipset Drivers Update

--- tasks/drivers/desktop_chipset_drivers.ps1
+++ tasks/drivers/desktop_chipset_drivers.ps1
@@ -15,8 +15,8 @@ $drivers = @(
     driverID = "$(pnputil /enum-devices /problem | Select-String 'VEN_8086')";
     driverRemote = "$networkSharePath\$gitRepoPath\files\desktop_drivers\Chipset.zip";
     sourceUnzipPath = "$driverTempPath\Chipset.zip";
-    destinationUnzipPath = "$driverTempPath\Chipset";
-    driverExe = "SetupChipset.exe";
+    destinationUnzipPath = "$driverTempPath\";
+    driverExe = "Chipset\SetupChipset.exe";
     installSwitches = "-s -norestart"
   }
 )
