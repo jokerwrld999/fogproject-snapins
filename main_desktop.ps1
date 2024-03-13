26d0c17ae5430491f0ed3f91adbe04b62df04853
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-13 11:09:45 +0200

    [helper_scripts] Add domain user Update

--- main_desktop.ps1
+++ main_desktop.ps1
@@ -4,7 +4,7 @@ param(
   [string] $networkUser,
   [Parameter(Mandatory = $true)]
   [string] $networkPass,
-  [Parameter(Mandatory = $true)]
+  [Parameter(Mandatory = $false)]
   [string] $domainMember,
   [Parameter(Mandatory = $false)]
   [string] $botToken,
@@ -21,7 +21,7 @@ net use $networkSharePath /user:$networkUser $networkPass
 powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\desktop_chipset_drivers.ps1" -networkSharePath $networkSharePath -gitRepoPath $gitRepoPath
 powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\nvidia.ps1"
 powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\software\scoop_packages.ps1"
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\\files\helper_scripts\add_domain_user.ps1" -domainMember $domainMember
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\files\helper_scripts\add_domain_user.ps1" -domainMember $domainMember
 & ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /HWID
 & ([ScriptBlock]::Create((irm "https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/files/helper_scripts/send_telegram_message.ps1"))) -botToken $botToken -chatID $chatID
 
