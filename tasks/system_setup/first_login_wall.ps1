bdf2775b1e2ed7df70ce9397d92054a9308000ec
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-15 15:02:43 +0200

    [first login] Powershell Task Arguments Fix...

--- tasks/system_setup/first_login_wall.ps1
+++ tasks/system_setup/first_login_wall.ps1
@@ -4,7 +4,7 @@ $scheduledTaskName = "SetLockscreenWallpaper"
 $getScheduledTaskName = (Get-ScheduledTask -TaskName $scheduledTaskName -ErrorAction SilentlyContinue).TaskName
 
 function ScheduleTaskForNextBoot () {
-  param ($scheduledTaskName)
+  # param ($scheduledTaskName)
   Write-Host "Scheduling task $scheduledTaskName for next boot..." -ForegroundColor Blue
 
   $ActionScript = '& {Invoke-Command -ScriptBlock ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString((New-Object Net.WebClient).DownloadData(''https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/tasks/system_setup/set_lockscreen_wallpaper.ps1''))))}'
@@ -19,13 +19,13 @@ function ScheduleTaskForNextBoot () {
   Register-ScheduledTask -TaskName "SetLockscreenWallpaper" -Action $Action -Trigger $Trigger -Principal $Principal -Description "Set Lockscreen Warning"
 }
 
-Invoke-RestMethod "https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/tasks/system_setup/set_lockscreen_wallpaper.ps1" | Invoke-Expression
+ powershell -ExecutionPolicy Bypass -NoProfile -Command "Invoke-RestMethod 'https://github.com/jokerwrld999/fogproject-snapins/raw/main/tasks/system_setup/set_lockscreen_wallpaper.ps1' | Invoke-Expression"
 
 if ($getScheduledTaskName -eq $scheduledTaskName) {
   Unregister-ScheduledTask -TaskName $scheduledTaskName -Confirm:$False -ErrorAction SilentlyContinue| Out-Null
-  ScheduleTaskForNextBoot -scheduledTaskName $scheduledTaskName
+  ScheduleTaskForNextBoot # -scheduledTaskName $scheduledTaskName
 } else {
-  ScheduleTaskForNextBoot -scheduledTaskName $scheduledTaskName
+  ScheduleTaskForNextBoot #-scheduledTaskName $scheduledTaskName
 }
 
 Stop-Transcript
