a3c79a372e967894a9eca378d3d1512a15a1eb32
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-20 11:31:29 +0200

    [alert] Test Sending Logs to Telegram #1

--- tasks/drivers/desktop_chipset_drivers.ps1
+++ tasks/drivers/desktop_chipset_drivers.ps1
@@ -24,7 +24,7 @@ $drivers = @(
     driverRemote = "$networkSharePath\$gitRepoPath\files\desktop_drivers\IOserial.zip";
     sourceUnzipPath = "$driverTempPath\IOserial.zip";
     destinationUnzipPath = $driverTempPath;
-    driverExe = "Chipset\AsusSetup.exe";
+    driverExe = "IOserial\AsusSetup.exe";
     installSwitches = "-s -norestart"
   }
 )
