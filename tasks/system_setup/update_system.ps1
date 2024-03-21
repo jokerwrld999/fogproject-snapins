1292baba1fc46eae8afce6d776aaeda176a7805d
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-21 16:00:02 +0200

    [PSUpdate] Nuget Added And Small fix

--- tasks/system_setup/update_system.ps1
+++ tasks/system_setup/update_system.ps1
@@ -9,6 +9,11 @@ $getUser = Get-LocalGroupMember -Name "Administrators" -Member $domainMember
 $languageTag = 'uk-UA'
 $languageList = Get-WinUserLanguageList
 
+if (!(Get-PackageProvider | Select-Object Name | Select-String "NuGet")) {
+  Write-Host "Installing NuGet Provider..." -ForegroundColor Blue
+  Install-PackageProvider -Name NuGet -Confirm:$False -Force
+}
+
 if (!(Get-Module -ListAvailable -Name $winUpdateModule)) {
   Install-Module -Name $winUpdateModule -Confirm:$False -Force | Out-Null
   Write-Host ("Installed module: $winUpdateModule") -ForegroundColor Green
@@ -18,7 +23,7 @@ Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -NotKBArticleID "KB5034441"
 
 if (![bool]$getUser) {
   Write-Host "Adding User..."
-  Add-LocalGroupMember -Name "Administrators" -Member $domainMember
+  Add-LocalGroupMember -Name "Administrators" -Member $domainMember -ErrorAction SilentlyContinue
 }
 
 if (![bool]($languageList | Where-Object LanguageTag -like $languageTag)) {
