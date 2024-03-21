8ae1fdf131e168e7a51f7caaaa40f82bef0cc288
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-21 17:44:16 +0200

    [snapins] Logs Added to Main Desktop

--- main_desktop.ps1
+++ main_desktop.ps1
@@ -15,15 +15,24 @@ param(
 $networkSharePath = "\\10.2.252.13\All\Department\Sysadmins\Fog"
 $gitRepoPath = "github\fogproject-snapins"
 $snapinScriptPath = "$networkSharePath\$gitRepoPath"
+$logsPath = "C:\Windows\Setup\Logs"
+
+if (!(Test-Path -Path $logsPath)) {
+  New-Item -Type Directory -Path $logsPath -Force | Out-Null
+}
+
+Start-Transcript -Path "$logsPath\0_main_snapin.txt"
 
 net use $networkSharePath /user:$networkUser $networkPass
 
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\desktop_chipset_drivers.ps1" -networkSharePath $networkSharePath -gitRepoPath $gitRepoPath
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\nvidia.ps1"
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\software\scoop_packages.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\desktop_chipset_drivers.ps1" -networkSharePath $networkSharePath -gitRepoPath $gitRepoPath | Out-File "$logsPath\1_chipset.txt"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\nvidia.ps1" | Out-File "$logsPath\2_nvidia.txt"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\software\scoop_packages.ps1"  | Out-File "$logsPath\3_scoop.txt"
 # powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\system_setup\update_system.ps1" -domainMember $domainMember
 & ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /HWID
 
 & ([ScriptBlock]::Create((irm "https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/tasks/system_setup/send_telegram_message.ps1"))) -botToken $botToken -chatID $chatID
 
 net use $networkSharePath /delete
+
+Stop-Transcript
