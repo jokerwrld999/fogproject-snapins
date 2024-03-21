d14811840b7da97cf98c17ddf6a82c395046510b
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-21 14:32:50 +0200

    [snapins] Main Snapins update

--- main_desktop copy.ps1
+++ main_desktop copy.ps1
@@ -19,11 +19,16 @@ $snapinScriptPath = "$networkSharePath\$gitRepoPath"
 net use $networkSharePath /user:$networkUser $networkPass
 
 $chipsetLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\desktop_chipset_drivers.ps1" -networkSharePath $networkSharePath -gitRepoPath $gitRepoPath)
-$nvidiaLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\nvidia.ps1")
-$scoopLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\software\scoop_packages.ps1")
+# $nvidiaLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\drivers\nvidia.ps1")
+# $scoopLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\software\scoop_packages.ps1")
 $domainMemberLog = $(powershell.exe -ExecutionPolicy Bypass -NoProfile -File "$snapinScriptPath\tasks\system_setup\update_system.ps1" -domainMember $domainMember)
-& ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /HWID
+# & ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /HWID
 
-& ([ScriptBlock]::Create((irm "https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/tasks/system_setup/send_telegram_message.ps1"))) -botToken $botToken -chatID $chatID
+$logMessage = "Your message is: " + $chipsetLog + "`n" + $nvidiaLog + "`n" + $scoopLog + "`n" + $domainMemberLog
+
+
+Write-Host $logMessage
+
+& ([ScriptBlock]::Create((irm "https://raw.githubusercontent.com/jokerwrld999/fogproject-snapins/main/tasks/system_setup/send_telegram_message.ps1"))) -botToken $botToken -chatID $chatID -logMessage $logMessage
 
 net use $networkSharePath /delete
